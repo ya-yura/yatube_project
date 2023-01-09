2f10631feaa59b118a276df687f7b4bc9e0bc818
Yuri Pilov <sharikov@gmail.com>
2023-01-09 15:32:29 +0300

    Sprint-3 First attempt of finalize

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=500)
+    slug = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def _str_(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='groups'
+    )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
-
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title
+    }
+    return render(request, 'posts/index.html', context)
 
 
-def group_show(request):
-    return HttpResponse('Список постов')
-
-
-def group_detail(request, pk):
-    return HttpResponse(f'Чота номер {pk}')
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
